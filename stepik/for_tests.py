381d9fe33eeeeecc3e7788a43cb16d0faf97c2de
LA-04 <lobanov0411@gmail.com>
2022-08-23 16:03:20 +0300

    7.2.1

--- stepik/for_tests.py
+++ stepik/for_tests.py
@@ -1,8 +1,5 @@
-def email(x):
-    if 'a' <= x.lower() <= 'z' or '0' <= x <= '9' or x  in '_@.':
-        print("ДА")
-    else:
-        print("НЕТ")
+def square(x):
+    return x*x
 
-
-email(input())
+a = float(input())
+print(square(a))
