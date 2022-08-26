2a75ad2bbda0a4dd09ef86eeb5f34cfb62e27378
LA-04 <lobanov0411@gmail.com>
2022-08-26 16:03:33 +0300

    7.2.7

--- stepik/for_tests.py
+++ stepik/for_tests.py
@@ -1,7 +1,9 @@
-tp = input()
-if tp == 'RECT':
-    def get_sq(a,b):
-        return a*b
-else:
-    def get_sq(a):
-        return a*a
+def how_long(words):
+    short_words = []
+    for word in words:
+        if len(word) >= 6:
+            short_words.append(word)
+    return short_words
+
+
+print(*how_long(input().split()))
