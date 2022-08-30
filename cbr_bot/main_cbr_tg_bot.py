f66792b2d95a85f7e29a8f2de0fb448d83e49c6c
LA-04 <lobanov0411@gmail.com>
2022-08-30 17:32:38 +0300

    'add aiogram_calendar'

--- cbr_bot/main_cbr_tg_bot.py
+++ cbr_bot/main_cbr_tg_bot.py
@@ -6,13 +6,24 @@ from aiogram.dispatcher import Dispatcher, FSMContext
 from aiogram.utils import executor
 from aiogram.dispatcher.filters.state import State, StatesGroup
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, CallbackQuery
+from aiogram_calendar import dialog_cal_callback, DialogCalendar
+
 
 
 storage = MemoryStorage()
 
 bot = Bot(token=token)
 dp = Dispatcher(bot, storage=storage)
+
+b1 = KeyboardButton('EUR')
+b2 = KeyboardButton('USD')
+b3 = KeyboardButton('JPY')
+b4 = KeyboardButton('CNY')
+
+kb_client = ReplyKeyboardMarkup(resize_keyboard=True)
+
+kb_client.add(b1).row(b2).add(b3).row(b4)
 
 class FSMAdmin(StatesGroup):
     valute = State()
@@ -21,14 +32,6 @@ class FSMAdmin(StatesGroup):
 #Начало диалога загрузки выбора валюты
 @dp.message_handler(commands=["start"], state=None)
 async def cm_start(message: types.Message):
-    b1 = KeyboardButton('EUR')
-    b2 = KeyboardButton('USD')
-    b3 = KeyboardButton('JPY')
-    b4 = KeyboardButton('CNY')
-
-    kb_client = ReplyKeyboardMarkup(resize_keyboard=True)
-
-    kb_client.add(b1).row(b2).add(b3).row(b4)
 
     await FSMAdmin.valute.set()
     await message.answer("Добрый день, выберете называние валюты:", reply_markup=kb_client)
@@ -36,25 +39,34 @@ async def cm_start(message: types.Message):
 #Получаем и сохраняем валюту
 @dp.message_handler(state=FSMAdmin.valute)
 async def valute_selection(message: types.Message, state: FSMContext):
-    b1 = KeyboardButton('2021/05/01')
-    b2 = KeyboardButton('2021/05/05')
-    b3 = KeyboardButton('2021/06/01')
-    b4 = KeyboardButton('2021/07/01')
 
-    kb_client = ReplyKeyboardMarkup(resize_keyboard=True)
-
-    kb_client.add(b1).row(b2).add(b3).row(b4)
     async with state.proxy() as data:
         data['valute'] = message.text
+
+    # b1 = KeyboardButton('2021/05/01')
+    # b2 = KeyboardButton('2021/05/05')
+    # b3 = KeyboardButton('2021/06/01')
+    # b4 = KeyboardButton('2021/07/01')
+    #
+    # kb_client = ReplyKeyboardMarkup(resize_keyboard=True)
+    #
+    # kb_client.add(b1).row(b2).add(b3).row(b4)
+
     await FSMAdmin.next()
-    await message.answer("А теперь дату", reply_markup=kb_client)
+    await message.answer("А теперь дату", reply_markup=await DialogCalendar().start_calendar())
 
 #Получаем и сохраняем желаемую дату
-@dp.message_handler(state=FSMAdmin.day)
-async def day_selection(message: types.Message, state: FSMContext):
+@dp.callback_query_handler(dialog_cal_callback.filter(), state=FSMAdmin.day)
+async def day_selection(callback_query: CallbackQuery, callback_data: dict, state: FSMContext):
+    selected, date = await DialogCalendar().process_selection(callback_query, callback_data)
+    if selected:
+        await callback_query.message.answer(
+            f'You selected {date.strftime("%Y/%m/%d")}'
+        )
+
     async with state.proxy() as data:
-        data['day'] = message.text
-    await message.answer("Секундочку...")
+        data['day'] = date.strftime("%Y/%m/%d")
+    await callback_query.message.answer("Секундочку...")
     async with state.proxy() as data:
         try:
             valute = data["valute"]
@@ -63,21 +75,11 @@ async def day_selection(message: types.Message, state: FSMContext):
             req = requests.get(f"https://www.cbr-xml-daily.ru/archive/{day}/daily_json.js")
             response = req.json()
             eur = response["Valute"][f"{valute}"]["Value"]
-            await message.reply(f"Дата: 2021/05/05\nКурс ЕВРО: {eur}")
+            await callback_query.message.answer(f"Дата: {day}\nКурс {valute}: {eur}", reply_markup=kb_client)
         except:
-            await message.reply("Проверьте валюту и дату")
+            await callback_query.message.reply("Ошибка, попробуйте еще раз", reply_markup=kb_client)
     await state.finish()
 
-#
-# @dp.message_handler()
-# async def get_valute(message: types.Message):
-#     try:
-#         req = requests.get(f"https://www.cbr-xml-daily.ru/archive/2021/05/05/daily_json.js")
-#         response = req.json()
-#         eur = response["Valute"][f"{message.text.upper()}"]["Value"]
-#         await message.reply(f"Дата: 2021/05/05\nКурс ЕВРО: {eur}")
-#     except:
-#         await message.reply("Проверьте валюту и дату")
 
 if __name__ == '__main__':
-    executor.start_polling(dp)
+    executor.start_polling(dp, skip_updates=True)
