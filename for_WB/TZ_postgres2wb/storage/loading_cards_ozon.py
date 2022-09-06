d17d92b9dbe48aeff37e4c9247c78239ca322f02
LA-04 <lobanov0411@gmail.com>
2022-09-06 17:53:40 +0300

    +

--- for_WB/TZ_postgres2wb/storage/loading_cards_ozon.py
+++ for_WB/TZ_postgres2wb/storage/loading_cards_ozon.py
@@ -5,7 +5,114 @@ import pymysql
 from config import host, user, password, db_name
 import requests
 from requests.structures import CaseInsensitiveDict
+from loguru import logger
 
+
+def upload_list(cursor):
+    select_all_rows = "SELECT * FROM upload_list;"
+    cursor.execute(select_all_rows)
+    rows = cursor.fetchall()
+    list_for_upload_ozon = []
+
+    for row in rows:
+        product_group = row.get("product")
+        upload_ozon = row.get("ozon")
+
+        if upload_ozon == 0:
+            list_for_upload_ozon.append(product_group)
+
+    logger.info("Список продуктов к загрузке сформирован...", 1)
+    return list_for_upload_ozon
+
+
+def request_on_ozon(list_for_upload):
+    t = {
+        "filter": {
+        "offer_id": [
+             "base_notepad_clear-0"
+             ],
+        "visibility": "ALL"
+        },
+        "limit": 100,
+        "sort_dir": "ASC"
+    }
+
+    p = t.get("filter")
+
+    for i, values in enumerate(list_for_upload):
+        p.update({"offer_id": [values]})
+        print(t)
+        url_1 = "https://api-seller.ozon.ru/v3/products/info/attributes"
+
+        headers = CaseInsensitiveDict()
+        headers["Client-Id"] = "287318"
+        headers["Api-Key"] = "0f6afa92-d026-47da-b951-d1198bf31ce7"
+        headers["Content-Type"] = "application/json"
+
+        data = f"""{json.dumps(t, indent=2, ensure_ascii=False)}""".encode('utf8')
+        resp = requests.post(url_1, headers=headers, data=data)
+        status = resp.status_code
+        if status == 200:
+            logger.info("Карточка с озона получена...", 1)
+            re_json = resp.json()
+            with open(f'request_to_oz\\{values}_oz.json', 'w', encoding='utf8') as dis_WB:
+                json.dump(re_json, dis_WB, indent=2, ensure_ascii=False)
+            # print(resp_json)
+        else:
+            logger.error(f"Ошибка {status} - Карточка с озона не получена...", 1)
+        return re_json
+
+
+def return_values(resp_json):
+    open_atrib = resp_json.get("result")
+    atrib = open_atrib[0].get("attributes")
+    # print(atrib)
+
+    with open('templates\\insert_card_oz.json', encoding='utf-8') as insert_card:
+        set_insert_card = json.load(insert_card)
+
+    open_atrib_insert = set_insert_card.get("result")
+    atrib_insert = open_atrib_insert[0].get("attributes")
+
+    for key, value_key in enumerate(atrib):
+        for ket_insert, value_key_insert in enumerate(atrib_insert):
+
+            if value_key['attribute_id'] == value_key_insert['attribute_id'] and value_key['values'] != \
+                    value_key_insert['values']:
+                value_key['values'] = value_key_insert['values']
+
+    # print(atrib)
+    open_atrib[0].update({"attributes": atrib})
+    resp_json.update({"result": open_atrib})
+    # print(resp_json)
+    logger.info("Замена значений произведена успешно...", 1)
+    return resp_json
+
+
+def send_to_ozon(return_json):
+    open_atrib = return_json.get("result")
+    atrib = open_atrib[0].get("attributes")
+    with open(f'atrib_cards_with_insert.json', 'w', encoding='utf8') as cards_with_insert:
+        json.dump(return_json, cards_with_insert, indent=2, ensure_ascii=False)
+    asd = {"attributes": atrib}
+    items = {"items": [asd]}
+    with open(f'items_cards_with_insert.json', 'w', encoding='utf8') as cards_with_insert:
+        json.dump(items, cards_with_insert, indent=2, ensure_ascii=False)
+    # print(items)
+    headers = CaseInsensitiveDict()
+    headers["Client-Id"] = "287318"
+    headers["Api-Key"] = "0f6afa92-d026-47da-b951-d1198bf31ce7"
+    headers["Content-Type"] = "application/json"
+
+    url_2 = "https://api-seller.ozon.ru/v2/product/import"
+    data = f"""{json.dumps(items, indent=2, ensure_ascii=False)}""".encode('utf8')
+
+    resp_ex = requests.post(url_2, headers=headers, data=data)
+    status = resp_ex.status_code
+    if status == 200:
+        logger.info("Обновленная карточка на озон отправлена...", 1)
+    else:
+        logger.error(f"Ошибка {status} - Обновленная карточка на озон не отправлена...", 1)
 
 
 try:
@@ -17,140 +124,21 @@ try:
         database=db_name,
         cursorclass=pymysql.cursors.DictCursor
     )
-    print("successfully connected...")
-    print("#" * 20)
+    logger.info("Подключение к бд успешно...", 1)
     try:
         with connection.cursor() as cursor:
 
-            # 1 пункт, проходим по листу загрузок, если не загружен, формируем список с продуктами к загрузке
-            select_all_rows = "SELECT * FROM upload_list;"
-            cursor.execute(select_all_rows)
-            rows = cursor.fetchall()
-            list_for_upload_ozon = []
+            list_for_upload = upload_list(cursor)
 
-            for row in rows:
-                product_group = row.get("product")
-                upload_ozon = row.get("ozon")
+            resp_json = request_on_ozon(list_for_upload)
 
-                if upload_ozon == 0:
-                    list_for_upload_ozon.append(product_group)
+            return_json = return_values(resp_json)
 
-            print("Список продуктов к загрузке сформирован...")
-            print("#" * 20)
-
-            # 2.2
-            # Заходим в таблицу wb_base_card для поиска пути джесона
-            select_all_rows = "SELECT * FROM wb_base_card;"
-            cursor.execute(select_all_rows)
-            rows = cursor.fetchall()
-
-            for row in rows:
-                product_type = row.get("product_type")
-                path_insert_json = row.get("path_insert_json")
-
-                for product_for_upload in list_for_upload_ozon:
-                    if product_type in product_for_upload:
-
-                        with open('templates\\testtempl.json', encoding='utf-8') as keys:
-                            set_keys = json.load(keys)
-
-                        with open(f'insert_fields\\insert_{product_type}.json', encoding='utf-8') as discription:
-                            set_discription = json.loads(discription.read()).get("addin")
-
-                        for num, value in enumerate(set_discription):
-                            params = value.get("params")
-                            repl = params[0]["value"]
-
-                            for key, value_key in reversed(set_keys.items()):
-                                if key in repl:
-                                    repl = repl.replace(f'{key}', value_key)
-
-                            value["params"][0].update({"value": repl})
-
-                        with open(f'product_with_keys\\{product_type}_with_keys.json', 'w', encoding='utf8') as dis_WB:
-                            json.dump(set_discription, dis_WB, ensure_ascii=False)
-            print("Files uploaded!")
-            print("#" * 20)
-
-            #отправка запроса на получение карточки из озона
-
-            url_1 = "https://api-seller.ozon.ru/v3/products/info/attributes"
-
-            headers = CaseInsensitiveDict()
-            headers["Client-Id"] = "287318"
-            headers["Api-Key"] = "0f6afa92-d026-47da-b951-d1198bf31ce7"
-            headers["Content-Type"] = "application/json"
-
-            t = """{
-                "filter": {
-                "offer_id": [
-                     "base_notepad_sketch-0"
-                     ],
-                "visibility": "ALL"
-                },
-                "limit": 100,
-                "sort_dir": "ASC"
-            
-            }"""
-
-            data = t
-
-            resp = requests.post(url_1, headers=headers, data=data)
-            status = resp.status_code
-            if status == 200:
-                print("Карточка с озона получена...")
-                print("#" * 20)
-                resp_json = resp.json()
-                #print(resp_json)
-            else:
-                print(f"Ошибка {status} - Карточка с озона не получена...")
-            open_atrib = resp_json.get("result")
-            atrib = open_atrib[0].get("attributes")
-            #print(atrib)
-
-            with open('templates\\insert_card_oz.json', encoding='utf-8') as insert_card:
-                set_insert_card = json.load(insert_card)
-
-            open_atrib_insert = set_insert_card.get("result")
-            atrib_insert = open_atrib_insert[0].get("attributes")
-
-            for key, value_key in enumerate(atrib):
-                for ket_insert, value_key_insert in enumerate(atrib_insert):
-
-                    if value_key['attribute_id'] == value_key_insert['attribute_id'] and value_key['values'] != value_key_insert['values']:
-                        value_key['values'] = value_key_insert['values']
-
-            #print(atrib)
-            open_atrib[0].update({"attributes":atrib})
-            resp_json.update({"result":open_atrib})
-            #print(resp_json)
-            print("Замена значений произведена успешно...")
-            print("#" * 20)
-
-            with open(f'atrib_cards_with_insert.json', 'w', encoding='utf8') as cards_with_insert:
-                json.dump(resp_json, cards_with_insert, indent=2, ensure_ascii=False)
-            asd = {"attributes":atrib}
-            items = {"items" : [asd]}
-            with open(f'items_cards_with_insert.json', 'w', encoding='utf8') as cards_with_insert:
-                json.dump(items, cards_with_insert, indent=2, ensure_ascii=False)
-            #print(items)
-
-            url_2 = "https://api-seller.ozon.ru/v2/product/import"
-            data = f"""{json.dumps(items, indent=2, ensure_ascii=False)}""".encode('utf8')
-
-            resp_ex = requests.post(url_2, headers=headers, data=data)
-            status = resp_ex.status_code
-            if status == 200:
-                print("Обновленная карточка на озон отправлена...")
-                print("#" * 20)
-            else:
-                print(f"Ошибка {status} - Обновленная карточка на озон не отправлена...")
-
-
+            send_json = send_to_ozon(return_json)
 
     finally:
         connection.close()
 
 except Exception as ex:
-    print("Connection refused...")
+    logger.error("Подключение к бд не удалось...", 1)
     print(ex)
