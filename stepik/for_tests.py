e2852789cfa678fd54e93c2af6927d9072e087b6
LA-04 <lobanov0411@gmail.com>
2022-09-15 12:54:27 +0300

    7.3.1

--- stepik/for_tests.py
+++ stepik/for_tests.py
@@ -1,6 +1,11 @@
-def min_max(min, max):
-    return min*max
+def get_nod(a, b):
+    if a < b:
+        a, b = b ,a
 
+    while b!=0:
+        a, b = b, a % b
+
+    return a
 
 numbers = list(map(int, input().split()))
-print(min_max(min(numbers), max(numbers)))
+print(get_nod(min(numbers), max(numbers)))
