24c89fee61436eab5de9ceec36d5f1b0e92e0ec2
LA-04 <lobanov0411@gmail.com>
2022-09-16 16:02:49 +0300

    7.4.4

--- stepik/for_tests.py
+++ stepik/for_tests.py
@@ -1,8 +1,11 @@
-def check_password(a, chars="$%!?@#"):
-    for i in a:
-        if len(a)>= 8 and i in chars:
-            return True
-    return False
-
+t = {'ё': 'yo', 'а': 'a', 'б': 'b', 'в': 'v', 'г': 'g', 'д': 'd', 'е': 'e', 'ж': 'zh',
+         'з': 'z', 'и': 'i', 'й': 'y', 'к': 'k', 'л': 'l', 'м': 'm', 'н': 'n', 'о': 'o', 'п': 'p',
+         'р': 'r', 'с': 's', 'т': 't', 'у': 'u', 'ф': 'f', 'х': 'h', 'ц': 'c', 'ч': 'ch', 'ш': 'sh',
+         'щ': 'shch', 'ъ': '', 'ы': 'y', 'ь': '', 'э': 'e', 'ю': 'yu', 'я': 'ya'}
+def check_password(a, sep = "-"):
+    lst = ''.join([t[i] if i in t else i for i in a].replace(' ', sep))
+    return lst
 #numbers = list(map(int, input().split()))
-print(check_password("12345678!"))
+str = input().lower()
+print(check_password(str))
+print(check_password(str, "+"))
