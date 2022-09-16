d83b1faddbaf0f4f88659aebc53dfe352b89e336
LA-04 <lobanov0411@gmail.com>
2022-09-16 16:18:50 +0300

    7.4.5

--- stepik/for_tests.py
+++ stepik/for_tests.py
@@ -1,11 +1,7 @@
-t = {'ё': 'yo', 'а': 'a', 'б': 'b', 'в': 'v', 'г': 'g', 'д': 'd', 'е': 'e', 'ж': 'zh',
-         'з': 'z', 'и': 'i', 'й': 'y', 'к': 'k', 'л': 'l', 'м': 'm', 'н': 'n', 'о': 'o', 'п': 'p',
-         'р': 'r', 'с': 's', 'т': 't', 'у': 'u', 'ф': 'f', 'х': 'h', 'ц': 'c', 'ч': 'ch', 'ш': 'sh',
-         'щ': 'shch', 'ъ': '', 'ы': 'y', 'ь': '', 'э': 'e', 'ю': 'yu', 'я': 'ya'}
-def check_password(a, sep = "-"):
-    lst = ''.join([t[i] if i in t else i for i in a].replace(' ', sep))
+def check_password(a, teg = "h1"):
+    lst = f"<{teg}>{a}</{teg}>"
     return lst
 #numbers = list(map(int, input().split()))
-str = input().lower()
+str = input()
 print(check_password(str))
-print(check_password(str, "+"))
+print(check_password(str, "div"))
