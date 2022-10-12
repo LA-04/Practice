f1f8c93d5e46ff63c2e38d6aa6b2ce18337a4778
LA-04 <lobanov0411@gmail.com>
2022-10-12 19:27:30 +0300

    A. Красивая матрица

--- Contest_YA/for_tests.py
+++ Contest_YA/for_tests.py
@@ -1,21 +1,18 @@
-def Bit(x, make):
-    k = 0
-    plus = "++"
-    mines = "--"
-    for i in range(x):
-        if plus in make[i][0].lower() and len(make[i][0]) == 3:
-            k += 1
-        elif mines in make[i][0].lower() and len(make[i][0]) == 3:
-            k -= 1
-    return k
+def matrix(m):
+    for i, item in enumerate(m):
+        for j, jtem in enumerate(item):
+            if jtem == 1:
+                row = abs(i - 2)
+                st = abs(j - 2)
+                return row + st
 
 
-x = int(input())
-make = []
-for i in range(x):
-    make.append(input().split())
+m = []
+for i in range(5):
+    m.append(list(map(int, input().split())))
 
-print(Bit(x, make))
+print(matrix(m))
+
 
 
 
