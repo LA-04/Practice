debe3b9fb74ead448757de92f24ba51b40b2e934
LA-04 <lobanov0411@gmail.com>
2022-10-12 19:50:27 +0300

    Циклический сдвиг

--- Contest_YA/for_tests.py
+++ Contest_YA/for_tests.py
@@ -1,18 +1,11 @@
-def matrix(m):
-    for i, item in enumerate(m):
-        for j, jtem in enumerate(item):
-            if jtem == 1:
-                row = abs(i - 2)
-                st = abs(j - 2)
-                return row + st
+def csickl_sdvig(a):
+    tmp = a[0]
+    for k in range(len(a) - 1):
+        a[k] = a[k + 1]
+    a[len(a) - 1] = tmp
+    return a
 
 
-m = []
-for i in range(5):
-    m.append(list(map(int, input().split())))
-
-print(matrix(m))
-
-
-
+a = [1,2,3,4,5]
+print(csickl_sdvig(a))
 
