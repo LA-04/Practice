dec23cb7638d629c7bf12f5402b2a7182a188de8
LA-04 <lobanov0411@gmail.com>
2022-10-12 18:03:08 +0300

    A. Укладка доминошками

--- Contest_YA/for_tests.py
+++ Contest_YA/for_tests.py
@@ -1,16 +1,12 @@
-def next_level(min_point, people):
-    k = 0
-    max_point = people[min_point[1]-1]
-    for i in range(len(people)):
-        if people[i] >= max_point and people[i] > 0:
-            k += 1
-    return k
+def domino(size):
+    return size[0] * size [1] // 2
 
-# min_point = list(map(int, input().split()))
-# people = list(map(int, input().split()))
+# size = list(map(int, input().split()))
 
-#print(next_level(min_point,people))
-print(next_level([8,5],[10,9,8,7,7,6,5,5]))
-print(next_level([4,2],[0,0,0,0]))
+
+#print(domino(size))
+print(domino([2,4]))
+print(domino([3,3]))
+print(domino([2,5]))
 
 
