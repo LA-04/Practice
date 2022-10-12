4c49187d267991e7ac8cf6e3ad7f8b5ca51c6bac
LA-04 <lobanov0411@gmail.com>
2022-10-12 17:53:55 +0300

    A. Следующий раунд

--- Contest_YA/for_tests.py
+++ Contest_YA/for_tests.py
@@ -1,15 +1,16 @@
-def team(task, who_know):
-    n = 0
-    for i in range(task):
-        if sum(who_know[i]) >= 2:
-            n += 1
-    return n
+def next_level(min_point, people):
+    k = 0
+    max_point = people[min_point[1]-1]
+    for i in range(len(people)):
+        if people[i] >= max_point and people[i] > 0:
+            k += 1
+    return k
+
+# min_point = list(map(int, input().split()))
+# people = list(map(int, input().split()))
+
+#print(next_level(min_point,people))
+print(next_level([8,5],[10,9,8,7,7,6,5,5]))
+print(next_level([4,2],[0,0,0,0]))
 
 
-task = int(input())
-who_know = []
-for i in range(task):
-    who_know.append(list(map(int, input().split())))
-
-print(team(task, who_know))
-print(team(2,[[1,0,0],[0,1,1]]))
