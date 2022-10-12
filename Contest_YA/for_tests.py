c4b91859fcea0589198102cef63e4c4b7fc8bab0
LA-04 <lobanov0411@gmail.com>
2022-10-12 18:28:22 +0300

    A. Bit++

--- Contest_YA/for_tests.py
+++ Contest_YA/for_tests.py
@@ -1,12 +1,21 @@
-def domino(size):
-    return size[0] * size [1] // 2
+def Bit(x, make):
+    k = 0
+    plus = "++"
+    mines = "--"
+    for i in range(x):
+        if plus in make[i][0].lower() and len(make[i][0]) == 3:
+            k += 1
+        elif mines in make[i][0].lower() and len(make[i][0]) == 3:
+            k -= 1
+    return k
 
-# size = list(map(int, input().split()))
 
+x = int(input())
+make = []
+for i in range(x):
+    make.append(input().split())
 
-#print(domino(size))
-print(domino([2,4]))
-print(domino([3,3]))
-print(domino([2,5]))
+print(Bit(x, make))
+
 
 
