9cb2aa9ec08ddbf0596017cb1d928bbf6e0f13c8
LA-04 <lobanov0411@gmail.com>
2022-10-13 13:58:38 +0300

    D. Строительство школы НЕ РЕШЕНА

--- Contest_YA/for_tests.py
+++ Contest_YA/for_tests.py
@@ -1,11 +1,22 @@
-def csickl_sdvig(a):
-    tmp = a[0]
-    for k in range(len(a) - 1):
-        a[k] = a[k + 1]
-    a[len(a) - 1] = tmp
-    return a
+def schools(students, coordinates):
+    if 0 < students < 100001:
+        middle = sum(coordinates)
+        if middle >= 0:
+            return middle // students
+        else:
+            return middle // students + 1
 
 
-a = [1,2,3,4,5]
-print(csickl_sdvig(a))
+# students = int(input())
+# coordinates = list(map(int, input().split()))
+#
+# print(schools(students, coordinates))
 
+print(schools(4,[1,2,3,4]))
+print(schools(3,[-1,0,1]))
+print(schools(3,[5,7,9]))
+print(schools(2,[0,1]))
+print(schools(3,[-100,0,1]))
+print(schools(0,[]))
+print(schools(3,[8,3,4]))
+print(schools(3,[-11,0,1]))
