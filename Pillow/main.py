a35e931a061314727400caddac82629f8405ef84
LA-04 <lobanov0411@gmail.com>
2022-11-22 22:32:26 +0300

    Score in game

--- Pillow/main.py
+++ Pillow/main.py
@@ -4,53 +4,104 @@ import os
 import random
 
 
-def create_rez(list_fon, list_rez):
+def create_rez_fem(list_fon, list_rez_fem):
     im_fon = Image.open(f'фон/{list_fon}')
-    im_rez = Image.open(f'результаты/фэм/{list_rez}')
-    im_rez_text = Image.open('маска/Rezult_text.png')
-    im_filter = Image.open('маска/filtr.png').convert("RGBA")
-    im_filter_small = Image.open('маска/filtr_small.png').convert("RGBA")
-    im_logo = Image.open('маска/logo_fem.png').convert("RGBA")
-    im_voda = Image.open('маска/zfemruolga.png').convert("RGBA")
-    im_under = Image.open('маска/zfemruolga_under.png').convert("RGBA")
+    im_rez_fem = Image.open(f'результаты/фэм/{list_rez_fem}')
+    im_rez_text = Image.open('маска/fem/Rezult_text.png')
+    im_filter = Image.open('маска/fem/filtr.png').convert("RGBA")
+    im_filter_small = Image.open('маска/fem/filtr_small.png').convert("RGBA")
+    im_logo = Image.open('маска/fem/logo_fem.png').convert("RGBA")
+    im_voda = Image.open('маска/fem/zfemruolga.png').convert("RGBA")
+    im_under = Image.open('маска/fem/zfemruolga_under.png').convert("RGBA")
 
 
     width, height = (800, 525)
     # print(im_mask.size)
-    width_rez, height_rez = im_rez.size
+    width_rez, height_rez = im_rez_fem.size
     # print(width_rez, height_rez)
     #im_fon = im_fon.filter(ImageFilter.BLUR)
     im_fon = im_fon.resize((width, height))
-    if height_rez < 65:
-        im_rez = im_rez.resize((width, 40))
+    if height_rez < 70:
+        im_rez_fem = im_rez_fem.resize((width, 40))
         im_voda.resize((width_rez, height_rez))
-        im_rez.paste(im_voda,(200,5), im_voda)
-    elif 65 <= height_rez <= 100:
-        im_rez = im_rez.resize((width, 60))
+        im_rez_fem.paste(im_voda, (200, 5), im_voda)
+    elif 70 <= height_rez <= 100:
+        im_rez_fem = im_rez_fem.resize((width, 60))
         im_voda.resize((width_rez, height_rez))
-        im_rez.paste(im_voda, (200,15), im_voda)
+        im_rez_fem.paste(im_voda, (200, 15), im_voda)
+    elif 100 < height_rez <= 120:
+        im_rez_fem = im_rez_fem.resize((width, 80))
+        im_voda.resize((width_rez, height_rez))
+        im_rez_fem.paste(im_voda, (200, 15), im_voda)
     else:
-        im_rez = im_rez.resize((width, 100))
+        im_rez_fem = im_rez_fem.resize((width, 100))
         im_voda.resize((width_rez, height_rez))
-        im_rez.paste(im_voda, (200,35), im_voda)
+        im_rez_fem.paste(im_voda, (200, 35), im_voda)
 
-    # print(im_rez.size)
+    # print(im_rez_fem.size)
     im_fon.paste(im_filter, (0, 0), im_filter)
     im_fon.paste(im_filter_small, (0, 110), im_filter_small)
-    im_fon.paste(im_rez, (0,height//2))
+    im_fon.paste(im_rez_fem, (0, height // 2))
     # im_fon.paste(im_voda,(10,290), im_voda)
     im_fon.paste(im_rez_text, (100, 146))
     im_fon.paste(im_logo, (25,30), im_logo)
     im_fon.paste(im_under, (25,485), im_under)
 
-    im_fon.save(f'rezults/{list_rez}+{list_fon}', quality = 95)
-    return logger.info(f"Картинка {list_fon}+{list_rez} создана")
+    im_fon.save(f'rezults/fem/{list_rez_fem}+{list_fon}', quality = 95)
+    return logger.info(f"Картинка {list_fon}+{list_rez_fem} создана")
 
+
+def create_rez_vitte(list_fon, list_rez_vitte):
+    im_fon = Image.open(f'фон/{list_fon}')
+    im_rez = Image.open(f'результаты/витте/{list_rez_vitte}')
+    im_stock = Image.open('маска/vitte/stock.png')
+    im_header = Image.open('маска/vitte/header.png').convert("RGBA")
+    im_filter = Image.open('маска/vitte/filtr.png').convert("RGBA")
+    im_voda = Image.open('маска/vitte/vittetests.png').convert("RGBA")
+    im_under = Image.open('маска/vitte/vittetests_under.png').convert("RGBA")
+
+    width, height = (800, 525)
+
+    width_rez, height_rez = im_rez.size
+
+    # im_fon = im_fon.resize((800, 400))
+    im_fon = im_fon.crop((0,100, 800, 500))
+
+    # if height_rez < 65:
+    #     im_rez = im_rez.resize((width, 40))
+    #     im_voda.resize((width_rez, height_rez))
+    #     im_rez.paste(im_voda,(200,5), im_voda)
+    # elif 65 <= height_rez <= 100:
+    #     im_rez = im_rez.resize((width, 60))
+    #     im_voda.resize((width_rez, height_rez))
+    #     im_rez.paste(im_voda, (200,15), im_voda)
+    # else:
+    #     im_rez = im_rez.resize((width, 100))
+    #     im_voda.resize((width_rez, height_rez))
+    #     im_rez.paste(im_voda, (200,35), im_voda)
+
+    im_rez = im_rez.resize((700, 150))
+    im_voda.resize((width_rez, height_rez))
+    im_rez.paste(im_voda, (200, 35), im_voda)
+
+    im_stock.paste(im_fon, (0, 100), im_fon)
+    im_stock.paste(im_filter, (0, 100), im_filter)
+    im_stock.paste(im_rez, (50,(height - 150 )//2))
+    im_stock.paste(im_header, (0, 0), im_header)
+    im_stock.paste(im_under, (25,505), im_under)
+
+    im_stock.save(f'rezults/vitte/{list_rez_vitte}+{list_fon}', quality = 95)
+    return logger.info(f"Картинка {list_fon}+{list_rez_vitte} создана")
 
 list_fon = os.listdir("фон/")
-list_rez = os.listdir("результаты/фэм/")
+list_rez_fem = os.listdir("результаты/фэм/")
+list_rez_vitte = os.listdir("результаты/витте/")
 
-for i in range(len(list_rez)):
+# for i in range(len(list_rez_fem)):
+#     random.shuffle(list_fon)
+#     create_rez_fem(random.choice(list_fon), list_rez_fem[i])
+
+for i in range(len(list_rez_vitte)):
     random.shuffle(list_fon)
-    create_rez(random.choice(list_fon), list_rez[i])
-
+    create_rez_vitte(random.choice(list_fon), list_rez_vitte[i])
+    # create_rez_vitte(random.choice(list_fon), list_rez_vitte[1])
--- stepik/for_tests.py
+++ stepik/for_tests.py
@@ -1,29 +1,74 @@
-def interactor(end_cod,verd_inter,verd_check):
-    checker = range(8)
-    inter = range(8)
-    info = range(-128,128)
-    if end_cod in info and verd_inter in inter and verd_check in checker:
-        if verd_inter == 0 and end_cod != 0:
-            return 3
-        elif verd_inter == 0:
-            return verd_check
-        if verd_inter == 1:
-            return verd_check
-        if verd_inter == 4 and end_cod != 0:
-            return 3
-        elif verd_inter == 4:
-            return 4
-        if verd_inter == 6:
-            return 0
-        if verd_inter == 7:
-            return 1
-        else:
-            return verd_inter
+from pprint import pprint
+import random
+import math
+
+TIMESTAMPS_COUNT = 50000
+
+PROBABILITY_SCORE_CHANGED = 0.0001
+
+PROBABILITY_HOME_SCORE = 0.45
+
+OFFSET_MAX_STEP = 3
+
+INITIAL_STAMP = {
+    "offset": 0,
+    "score": {
+        "home": 0,
+        "away": 0
+    }
+}
 
 
-print(interactor(0,0,0))
-print(interactor(-1,0,1))
-print(interactor(42,1,6))
-print(interactor(44,7,4))
-print(interactor(1,4,0))
-print(interactor(-3,2,4))
+def generate_stamp(previous_value):
+    score_changed = random.random() > 1 - PROBABILITY_SCORE_CHANGED
+    home_score_change = 1 if score_changed and random.random() > 1 - \
+        PROBABILITY_HOME_SCORE else 0
+    away_score_change = 1 if score_changed and not home_score_change else 0
+    offset_change = math.floor(random.random() * OFFSET_MAX_STEP) + 1
+
+    return {
+        "offset": previous_value["offset"] + offset_change,
+        "score": {
+            "home": previous_value["score"]["home"] + home_score_change,
+            "away": previous_value["score"]["away"] + away_score_change
+        }
+    }
+
+
+def generate_game():
+    stamps = [INITIAL_STAMP, ]
+    current_stamp = INITIAL_STAMP
+    for _ in range(TIMESTAMPS_COUNT):
+        current_stamp = generate_stamp(current_stamp)
+        stamps.append(current_stamp)
+
+    return stamps
+
+
+def get_score(game_stamps, offset):
+    '''
+        Takes list of game's stamps and time offset for which returns the scores for the home and away teams.
+        Please pay attention to that for some offsets the game_stamps list may not contain scores.
+    '''
+    goals = [INITIAL_STAMP, ]
+    home = 0
+    away = 0
+    for game in game_stamps:
+        if game["score"]["home"] != home or game["score"]["away"] != away:
+            goals.append(game)
+            home = game["score"]["home"]
+            away = game["score"]["away"]
+    goals.append(game_stamps[-1])
+    for id, goal in enumerate(goals):
+        if offset < goal["offset"]:
+            return goals[id-1]["score"]["home"], goals[id-1]["score"]["away"]
+        if offset == goal["offset"]:
+            return goals[id]["score"]["home"], goals[id]["score"]["away"]
+
+game_stamps = generate_game()
+print(game_stamps)
+
+last_offset = game_stamps[-1]["offset"]
+offset = int(input(f'Enter a number in the range from 0 to {last_offset}: '))
+home, away = get_score(game_stamps, offset)
+print(f"Home {home}:{away} Away")
